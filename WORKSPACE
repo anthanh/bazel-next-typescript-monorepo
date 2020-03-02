8d15f1a6dd1a48d63188cef2bef24bc45d81e870
Anthanh <antai.pt@gmail.com>
2020-03-02 10:02:42 +0100

    chore(webapp2): ts_setup_workspace added

--- WORKSPACE
+++ WORKSPACE
@@ -23,3 +23,6 @@ yarn_install(
 load("@npm//:install_bazel_dependencies.bzl", "install_bazel_dependencies")
 
 install_bazel_dependencies()
+
+load("@npm_bazel_typescript//:index.bzl", "ts_setup_workspace")
+ts_setup_workspace()
--- packages/webapp2/pages/index.tsx
+++ packages/webapp2/pages/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+// import React from 'react'
 import doWhatever from '@monorepo/common2'
 
 function HomePage() {
-  return <p>{doWhatever('Welcome to Next.js!')}</p>
+  // return <p>{doWhatever('Welcome to Next.js!')}</p>
+  return doWhatever('Welcome to Next.js!')
 }
 
 export default HomePage
